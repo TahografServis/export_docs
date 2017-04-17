881eb73c3a8ee6c568d851976e04234af6dc29c9
ProjectSoft <projectsoft2009@yandex.ru>
2017-04-17 05:52:19 +0400

    add

--- src/js/main.js
+++ src/js/main.js
@@ -1,9 +1,14 @@
 $(function(){
-	$('pre.code').highlight({
-		source:true,
-		zebra:true,
-		indent:'tabs',
-		list:'ol'
+	$('pre.code').each(function(){
+		var txt = $(this).text().length;
+		if(txt>10){
+			$(this).highlight({
+				source:true,
+				zebra:true,
+				indent:'tabs',
+				list:'ol'
+			});
+		}
 	});
 	$('td:nth-child(3)').hyphenate('ru');
 	$('p').hyphenate('en-us');
